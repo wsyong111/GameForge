5039aa4ef5bf847ce9153dd7821657c94597ceef
wsyong111 <wsyong11@gmail.com>
2026-03-04 20:07:30 +0800

    wsyong11Pc

--- Framework/System/Audio/src/main/java/io/github/wsyong11/gameforge/framework/system/audio/impl/simple/audio/AudioDecoderMap.java
+++ Framework/System/Audio/src/main/java/io/github/wsyong11/gameforge/framework/system/audio/impl/simple/audio/AudioDecoderMap.java
@@ -1,14 +1,21 @@
 package io.github.wsyong11.gameforge.framework.system.audio.impl.simple.audio;
 
 import io.github.wsyong11.gameforge.framework.mime.MimeType;
+import io.github.wsyong11.gameforge.framework.system.audio.audio.decoder.AudioDecodeHint;
 import io.github.wsyong11.gameforge.framework.system.audio.audio.decoder.AudioDecoderFactory;
+import io.github.wsyong11.gameforge.framework.system.log.Log;
+import io.github.wsyong11.gameforge.framework.system.log.Logger;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.jetbrains.annotations.Unmodifiable;
+import org.jetbrains.annotations.UnmodifiableView;
 
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class AudioDecoderMap {
+	private static final Logger LOGGER = Log.getLogger();
+
 	private final List<AudioDecoderFactory> list;
 	private final Map<AudioDecoderFactory, FactoryInfo> infoMap;
 	private final Map<MimeType, List<FactoryInfo>> cacheMap;
@@ -18,8 +25,8 @@ public class AudioDecoderMap {
 	public AudioDecoderMap() {
 		this.list = new ArrayList<>();
 
-		this.infoMap = new HashMap<>();
-		this.cacheMap = new HashMap<>();
+		this.infoMap = new IdentityHashMap<>();
+		this.cacheMap = new ConcurrentHashMap<>();
 
 		this.lock = new Object();
 	}
@@ -29,35 +36,97 @@ public class AudioDecoderMap {
 	public List<FactoryInfo> get(@NotNull MimeType mimeType) {
 		Objects.requireNonNull(mimeType, "mimeType is null");
 
-		List<FactoryInfo> cacheList = this.cacheMap.get(mimeType);
-		if (cacheList != null)
-			return cacheList;
-
-		synchronized (this.lock) {
-			List<FactoryInfo> list = this.find(mimeType);
-			this.cacheMap.putIfAbsent(mimeType, list);
-			return list;
-		}
+		return this.cacheMap.computeIfAbsent(mimeType, this::find);
 	}
 
+	@NotNull
 	private List<FactoryInfo> find(@NotNull MimeType mimeType) {
+		Objects.requireNonNull(mimeType, "mimeType is null");
 
+		List<AudioDecoderFactory> list;
+		Map<AudioDecoderFactory, FactoryInfo> infoMap;
+		synchronized (this.lock) {
+			list = List.copyOf(this.list);
+			infoMap = Map.copyOf(this.infoMap);
+		}
+
+		return list
+			.stream()
+			.map(infoMap::get)
+			.filter(Objects::nonNull)
+			.filter(info -> info
+				.getSupportMimes()
+				.stream()
+				.anyMatch(mime -> mime.includes(mimeType)))
+			.toList();
 	}
 
 	public void register(@NotNull AudioDecoderFactory factory) {
 		Objects.requireNonNull(factory, "factory is null");
 
+		synchronized (this.lock) {
+			if (this.list.contains(factory))
+				return;
 
-		this.list.add(factory);
+			Set<MimeType> supportMimes = factory.getSupportMimes();
+			Set<AudioDecodeHint> supportHints = factory.getSupportHints();
+
+			FactoryInfo info = new FactoryInfo(factory, supportMimes, supportHints);
+
+			this.infoMap.put(factory, info);
+			this.list.add(factory);
+		}
 	}
 
 	public void unregister(@NotNull AudioDecoderFactory factory) {
 		Objects.requireNonNull(factory, "factory is null");
 
-		this.list.remove(factory);
+		synchronized (this.lock) {
+			if (!this.list.remove(factory))
+				return;
+
+			FactoryInfo info = this.infoMap.remove(factory);
+			for (Map.Entry<MimeType, List<FactoryInfo>> entry : List.copyOf(this.cacheMap.entrySet())) {
+				MimeType type = entry.getKey();
+				List<FactoryInfo> infos = entry.getValue();
+				if (!infos.contains(info))
+					continue;
+
+				this.cacheMap.remove(type);
+			}
+		}
 	}
 
 	public static class FactoryInfo {
+		private final AudioDecoderFactory factory;
+		private final Set<MimeType> supportMimes;
+		private final Set<AudioDecodeHint> supportHints;
 
+		public FactoryInfo(@NotNull AudioDecoderFactory factory, @NotNull Set<MimeType> supportMimes, @NotNull Set<AudioDecodeHint> supportHints) {
+			Objects.requireNonNull(factory, "factory is null");
+			Objects.requireNonNull(supportMimes, "supportMimes is null");
+			Objects.requireNonNull(supportHints, "supportHints is null");
+
+			this.factory = factory;
+			this.supportMimes = Set.copyOf(supportMimes);
+			this.supportHints = Set.copyOf(supportHints);
+		}
+
+		@NotNull
+		public AudioDecoderFactory getFactory() {
+			return this.factory;
+		}
+
+		@NotNull
+		@UnmodifiableView
+		public Set<MimeType> getSupportMimes() {
+			return this.supportMimes;
+		}
+
+		@NotNull
+		@UnmodifiableView
+		public Set<AudioDecodeHint> getSupportHints() {
+			return this.supportHints;
+		}
 	}
 }
