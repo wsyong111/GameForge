523ff8202e3ed803e0e57e6eed41a68fbaed2bc5
wsyong11 <wsyong11@gmail.com>
2026-04-14 14:44:51 +0800

    wsyong11Tablet

--- Framework/SPI/src/main/java/io/github/wsyong11/gameforge/framework/spi/registry/SimpleExtensionRegistry.java
+++ Framework/SPI/src/main/java/io/github/wsyong11/gameforge/framework/spi/registry/SimpleExtensionRegistry.java
@@ -1,6 +1,7 @@
 package io.github.wsyong11.gameforge.framework.spi.registry;
 
 import io.github.wsyong11.gameforge.framework.spi.ExtensionType;
+import io.github.wsyong11.gameforge.util.IdentityRef;
 import io.github.wsyong11.gameforge.util.collection.CollectionUtils;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -8,43 +9,95 @@ import org.jetbrains.annotations.Unmodifiable;
 
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class SimpleExtensionRegistry implements ExtensionRegistry {
 	private final Map<ExtensionType<?>, Set<ExtensionEntry<?>>> extensionMap;
+	private final Set<IdentityRef<?>> allExtensions;
 
-//	private final ReadWriteLock lock;
+	private final ReadWriteLock lock;
 
 	public SimpleExtensionRegistry() {
 		this.extensionMap = new ConcurrentHashMap<>();
-//		this.lock = new ReentrantReadWriteLock();
+		this.allExtensions = new LinkedHashSet<>();
+
+		this.lock = new ReentrantReadWriteLock();
 	}
 
 	@NotNull
 	protected <T> Set<ExtensionEntry<T>> getExtensionSet(@NotNull ExtensionType<T> type) {
 		Objects.requireNonNull(type, "type is null");
-		return CollectionUtils.forceCast(this.extensionMap.computeIfAbsent(type, k ->
-			Collections.synchronizedSet(new LinkedHashSet<>())));
+		return CollectionUtils.forceCast(this.extensionMap.computeIfAbsent(type, k -> new LinkedHashSet<>()));
 	}
 
 	@Override
 	public <T> void register(@NotNull ExtensionType<T> type, @NotNull T instance) {
 		Objects.requireNonNull(type, "type is null");
+		Objects.requireNonNull(instance, "instance is null");
 
-		new ExtensionEntry<>()
+		ExtensionEntry<T> entry = new ExtensionEntry<>(instance);
 
-		Set<ExtensionEntry<T>> extensions = this.getExtensionSet(type);
-		extensions.contains()
+		Lock lock = this.lock.writeLock();
+		lock.lock();
+		try {
+			Set<ExtensionEntry<T>> extensions = CollectionUtils.forceCast(
+				this.extensionMap.computeIfAbsent(type,
+					k -> new LinkedHashSet<>()));
+
+			if (!extensions.add(entry))
+				throw new IllegalArgumentException("Extension " + instance + " is registered");
+
+			this.allExtensions.add(entry);
+		} finally {
+			lock.unlock();
+		}
 	}
 
 	@Override
 	public <T> void unregister(@NotNull ExtensionType<T> type, @NotNull T instance) {
+		Objects.requireNonNull(type, "type is null");
+		Objects.requireNonNull(instance, "instance is null");
 
+		ExtensionEntry<T> entry = new ExtensionEntry<>(instance);
+
+		Lock lock = this.lock.writeLock();
+		lock.lock();
+		try {
+			Set<ExtensionEntry<T>> extensions = CollectionUtils.forceCast(this.extensionMap.get(type));
+			if (extensions == null)
+				return;
+
+			extensions.remove(entry);
+			if (extensions.isEmpty())
+				this.extensionMap.remove(type);
+		} finally {
+			lock.unlock();
+		}
 	}
 
 	@Override
 	public <T> void unregister(@NotNull T instance) {
+		Objects.requireNonNull(instance, "instance is null");
 
+		ExtensionEntry<T> entry = new ExtensionEntry<>(instance);
+
+		Lock lock = this.lock.writeLock();
+		lock.lock();
+		try {
+			for (Map.Entry<ExtensionType<?>, Set<ExtensionEntry<?>>> e : List.copyOf(this.extensionMap.entrySet())) {
+				Set<ExtensionEntry<?>> extensions = e.getValue();
+				extensions.remove(entry);
+
+				if (extensions.isEmpty())
+					this.extensionMap.remove(e.getKey());
+			}
+		} finally {
+			lock.unlock();
+		}
 	}
+
 
 	@Override
 	public <T> boolean has(@NotNull T extension) {
@@ -86,19 +139,12 @@ public class SimpleExtensionRegistry implements ExtensionRegistry {
 
 	}
 
-	protected static class ExtensionEntry<T> implements Comparable<ExtensionEntry<T>> {
-		private final T instance;
+	protected static class ExtensionEntry<T> extends IdentityRef<T> implements Comparable<ExtensionEntry<T>> {
 		private volatile int priority;
 
 		private ExtensionEntry(@NotNull T instance) {
-			Objects.requireNonNull(instance, "instance is null");
-			this.instance = instance;
+			super(instance);
 			this.priority = 0;
-		}
-
-		@NotNull
-		public T getInstance() {
-			return this.instance;
 		}
 
 		public void setPriority(int priority) {
