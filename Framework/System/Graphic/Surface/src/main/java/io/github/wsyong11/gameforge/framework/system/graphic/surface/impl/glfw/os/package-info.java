9906323e8e8e211fcf1feb106d4cd04d27adcc3a
wsyong11 <wsyong11@gmail.com>
2026-05-21 14:55:02 +0800

    wsyong11Tablet

--- Framework/System/Graphic/Surface/src/main/java/io/github/wsyong11/gameforge/framework/system/graphic/surface/impl/glfw/os/package-info.java
+++
@@ -1,4 +0,0 @@
-@UnsafeAPI
-package io.github.wsyong11.gameforge.framework.system.graphic.surface.impl.glfw.os;
-
-import io.github.wsyong11.gameforge.framework.annotation.UnsafeAPI;
--- Framework/System/Graphic/Surface/src/main/java/io/github/wsyong11/gameforge/framework/system/graphic/surface/window/monitor/Monitor.java
+++ Framework/System/Graphic/Surface/src/main/java/io/github/wsyong11/gameforge/framework/system/graphic/surface/window/monitor/Monitor.java
@@ -1,4 +1,10 @@
 package io.github.wsyong11.gameforge.framework.system.graphic.surface.window.monitor;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.util.UUID;
+
 public interface Monitor {
+	@NotNull
+	UUID getId();
 }
